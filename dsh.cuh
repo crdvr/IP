e1ae8bace6ace0f63c0c70563ddf41f64ad955e8
crdvr <moduli@outlook.jp>
2017-08-22 16:52:31 +0900

    a

--- dsh.cuh
+++ dsh.cuh
@@ -1,47 +1,44 @@
-void dsf(float *a,float *b,int w,int h,int n0,int n1){
-	for(int i=0;i<h/n1;i++){
-		for(int j=0;j<w/n0;j++){
-			float v=0;
-			for(int k=0;k<n1;k++){
-				for(int l=0;l<n0;l++){
-					v+=b[((n1*i+k)<h?(n1*i+k):h-1)*w+(n0*j+l<w?n0*j+l:w-1)];
 
-				}
-			}
-			a[i*(w/n0)+j]=v/n1/n0;
-		}
-	}
+#define AR3 printf("(e:%d,d:%d)",e,d);a[i*w1+j]+=u*v*b[(d)*w+e];
+
+#define REP3(a,b,c,d,e,f,h,i,g) for(int a=h*i;a<OVU(h,i,c);a++){if(a*b/c==(a+1)*b/c){e=(a*b/c);f=((float)b/c);g;}else{if(a*b%c!=0){\
+	e=(a*b/c);f=a*b/c+1-(float)a*b/c;g;}
+else{ e=(a*b/c);f=1;g; }for(int d=a*b/c+1;d<(a+1)*b/c;d++){
+	\
+		e=d;f=1;g;
+}if((a+1)*b%c!=0){ e=(a+1)*b/c;f=(float)(a+1)*b/c-(a+1)*b/c;g; }}}
+
+#define LOAD printf("(l:%d,k:%d)\n",l,k);rdrh2(b,s,(l*X*w/w1),(OVU(l,X,w1)*w%w1!=0?OVU(l,X,w1)*w/w1+1:OVU(l,X,w1)*w/w1),(k*Y*h/h1),(OVU(k,Y,h1)*h%h1!=0?OVU(k,Y,h1)*h/h1+1:OVU(k,Y,h1)*h/h1),w,h);printf("suc\n");//for(int ind=0;ind<w*h;ind++){b[ind]=1;}
+//
+#define FT printf("\n");//printf("%f %f %f %f",a[0],a[1],a[2],a[3]);
+void dsf2d(float *a,float *b,int w,int h,int w1,int h1,char *s){
+	for(int i=0;i<w1*h1;i++)a[i]=0;
+	int X=2,Y=2,k,l;
+
+	int d,e;
+	float u,v;
+	REPREM(k,h1,Y,REPREM(l,w1,X,LOAD REP3(i,h,h1,p,d,u,k,Y,REP3(j,w,w1,q,e,v,l,X,AR2)) FT));
+
+
+	for(int i=0;i<w1*h1;i++)a[i]/=(float)w/w1*h/h1;
+
 }
 
-void wtrdh(float *a,char *fn,int v){//write data in device
-	int i;
-	FILE *fp;
-	char name[250];
-	float *b;
-	//MAL(b,float,v);
-	//CUCP(b,a,float,v,D2H);
-	myf(&fp,fn,"wb");
-	fwrite(a,4,v,fp);
-	fclose(fp);
-	//free(b);
-#ifdef PRT
-	printf("writing %s finished\n",fn);
-#else
 
-#endif
-}
 
 void main(int argc,char *argv[]){
-	int w,h,n0,n1;
-	float *arr,*b;
+	int w,h,n0,n1,w1,h1;
+	float *arr,*b,*c;
 	printf("w:");scanf("%d",&w);printf("h:");scanf("%d",&h);
 
-	printf("n0:");scanf("%d",&n0);printf("h:");scanf("%d",&n1);
+	//printf("n0:");scanf("%d",&n0);printf("n1:");scanf("%d",&n1);
+	printf("w1:");scanf("%d",&w1);printf("h1:");scanf("%d",&h1);
 	MAL(arr,float,w*h);
-	MAL(b,float,w/n0*(h/n1));
+	MAL(b,float,w1*h1);
 	rdr(&arr,argv[1],2,w,h,0,0);
-	dsf(b,arr,w,h,n0,n1);
+	dsf2d(b,arr,w,h,w1,h1,argv[1]);
 	char s[256];
-	sprintf(s,"%s-ds%d-%d",argv[1],n0,n1);
-	wtrdh(b,s,w/n0*(h/n1));
+	sprintf(s,"%s-ds-%d-%d",argv[1],w1,h1);
+	wtrdh(b,s,w1*h1);
+	int i;scanf("%d",&i);
 }
